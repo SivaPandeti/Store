61c0bf84593ae986f0a08d53f7567a9f3645be38
Siva Pandeti <psivah@yahoo.com>
2013-03-26 14:45:53 -0400

    Added button

--- Store/AppDelegate.m
+++ Store/AppDelegate.m
@@ -13,6 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    NSLog(@"App did Finish Launching!");
     return YES;
 }
 							
