f9e48ca63579ecc77f434cbe30c64a72a809057c
Siva Pandeti <psivah@yahoo.com>
2013-03-26 11:11:47 -0400

    Initial Commit

---
+++ Store/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Store
+//
+//  Created by span on 3/26/13.
+//  Copyright (c) 2013 DustRise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Store/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Store
+//
+//  Created by span on 3/26/13.
+//  Copyright (c) 2013 DustRise. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
